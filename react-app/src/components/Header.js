e2da34ae2207b79e9690a341beac7333092416d8
Fraser Dwyer <fraserd1413@gmail.com>
2024-11-10 14:58:18 +0000

    Link placeholders added

--- react-app/src/components/Header.js
+++ react-app/src/components/Header.js
@@ -1,8 +1,15 @@
 export default function Header() {
   return (
     <div className="headerContainer">
-      <img src="/logo.png" alt="Coffee Beans" className="logo" />
-      <p>D3JS Coffee Dashboard</p>
+      <div className="logoContainer">
+        <img src="/logo.png" alt="Coffee Beans" />
+        <p>D3JS Coffee Dashboard</p>
+      </div>
+      <div className="linkContainer">
+        <p>Home</p>
+        <p>About</p>
+        <p>Contact</p>
+      </div>
     </div>
   );
 }
